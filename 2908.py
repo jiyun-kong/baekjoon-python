b5b2b8b9e4e90115980f7b17582f1de75400ea3c
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-01-19 11:28:47 +0900

    상수 (숫자를 뒤집어서 비교하는 문제)
    - [::-1] : 역순

--- 2908.py
+++ 2908.py
@@ -1,7 +1,8 @@
-a, b = input().split()
-a = list(a)
-a = a.reverse()
-b = list(b)
-b = b.reverse()
+num1, num2 = input().split()
+num1 = int(num1[::-1])  # 
+num2 = int(num2[::-1])
 
-print(a)
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)

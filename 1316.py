de0883475b06b04c6521dca412e208352c2860f4
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-01-22 12:17:54 +0900

    그룹 단어 체커
    - 특히 리스트 다룰 때, 리스트 인덱스 끝까지 꼼꼼하게 따지기. 자꾸 런타임 에러가 발생하는 이유임.

--- 1316.py
+++ 1316.py
@@ -1,4 +1,30 @@
 testCase = int(input())
+count = 0
 
 for t in range(testCase):
-    str = list(input())
+    _str = list(input())
+    res = True                  # True : 기본값 / False : 그룹 단어가 아님
+
+    i = 0
+
+    while True:
+        last_idx = len(_str) - 1
+
+        if i == last_idx:
+            break
+        else:
+            if _str[i] == _str[i+1]:
+                del _str[i]
+            else:
+                i += 1
+
+    for k in range(len(_str)-1):
+        for j in range(k+1, len(_str)):
+            if _str[k] == _str[j]:
+                res = False
+                break
+
+    if res == True:
+        count += 1
+
+print(count)

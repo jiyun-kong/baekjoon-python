89db30976ef40a928608751f50b2ee433267f342
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-01-25 10:20:24 +0900

    벌집 (벌집이 형성되는 규칙에 따라 벌집의 위치를 구하는 문제)
    - 규칙을 파악한 것에서 식을 도출해본다.
    - 누적합을 잘 활용하자.

--- 2292.py
+++ 2292.py
@@ -1,1 +1,22 @@
 n = int(input())
+count = 0
+
+if n == 1:
+    count = 1
+else:
+    i = -1
+    middleSum = 2
+
+    while True:
+        i += 1
+        if n > (middleSum + 6 * i):
+            middleSum += 6 * i
+            continue
+        elif n == (middleSum + 6 * i):
+            count = i + 2
+            break
+        else:
+            count = i + 1
+            break
+
+print(count)

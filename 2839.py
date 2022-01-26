0246cad1c527a1639133c93f4e9a023e1cf9bbce
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-01-26 15:16:58 +0900

    설탕 배달 (5와 3을 최소 횟수로 합하여 N을 만드는 문제)
    - 동적 프로그래밍 중에서 바텀업으로 메모이제이션을 사용했다.
    - 최댓값이 1001이라고 생각해서 dp 배열을 1001로 초기화했는데 틀렸다고 계속 나왔다.
    - from math import inf 해서 inf 무한 값으로 초기화했더니 정답이었다.

---
+++ 2839.py
@@ -0,0 +1,13 @@
+from math import inf
+n = int(input())
+
+dp = [inf] * 5001
+dp[3] = dp[5] = 1
+
+for i in range(6, n+1):
+    dp[i] = min(dp[i-3] + 1, dp[i-5] + 1)
+
+if dp[n] != inf:
+    print(dp[n])
+else:
+    print(-1)

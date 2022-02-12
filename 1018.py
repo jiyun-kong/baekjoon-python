31f0a321281e2bb1ebd2707da63c9f0ca683ab01
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-02-12 11:58:37 +0900

    체스판 다시 칠하기

--- 1018.py
+++ 1018.py
@@ -1,13 +1,51 @@
 saero, garo = map(int, input().split())
 lst = []            # 입력된 보드의 상태를 저장하는 리스트
-errorStorage = []   # 몇 개의 원소를 고쳐야 하는지 저장하는 리스트
-idx = 0             # wb 리스트를 확인 할 인덱스
-
-wb = ['w', 'b']
+errorStorage = []   # 몇 개의 원소를 고쳐야 하는지 저장하는 리스트 -> 나중에 min(errorStorage)
 
 # 입력받기
 for i in range(saero):
     lst.append(input())
 
+# 검토하기
 for i in range(0, saero-7):
     for j in range(0, garo-7):
+        count = 0
+
+        for y in range(i, i+8):
+            for x in range(j, j+8):
+                if (y-i) % 2 == 0:
+                    if (x-j) % 2 == 0:
+                        if lst[y][x] == 'B':
+                            count += 1
+                    else:
+                        if lst[y][x] == 'W':
+                            count += 1
+                else:
+                    if (x-j) % 2 == 0:
+                        if lst[y][x] == 'W':
+                            count += 1
+                    else:
+                        if lst[y][x] == 'B':
+                            count += 1
+        errorStorage.append(count)
+
+        count = 0
+        for y in range(i, i+8):
+            for x in range(j, j+8):
+                if (y-i) % 2 == 0:
+                    if (x-j) % 2 == 0:
+                        if lst[y][x] == 'W':
+                            count += 1
+                    else:
+                        if lst[y][x] == 'B':
+                            count += 1
+                else:
+                    if (x-j) % 2 == 0:
+                        if lst[y][x] == 'B':
+                            count += 1
+                    else:
+                        if lst[y][x] == 'W':
+                            count += 1
+        errorStorage.append(count)
+
+print(min(errorStorage))

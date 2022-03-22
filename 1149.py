0f0ab7122c9b33dab80cb7b564e88de5d6d05f43
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-03-22 18:22:11 +0900

    RGB 거리
    - 메모이제이션을 사용하는데 특이하게 i번째 기준으로 i-1 값을 비교하는 문제였음

--- 1149.py
+++ 1149.py
@@ -5,4 +5,14 @@ for i in range(n):
     lst = list(map(int, input().split(' ')))
     rgb.append(lst)
 
-print(rgb)
+for i in range(1, n):
+    # R
+    rgb[i][0] = min(rgb[i-1][1], rgb[i-1][2]) + rgb[i][0]
+
+    # G
+    rgb[i][1] = min(rgb[i-1][0], rgb[i-1][2]) + rgb[i][1]
+
+    # B
+    rgb[i][2] = min(rgb[i-1][0], rgb[i-1][1]) + rgb[i][2]
+
+print(min(rgb[n-1]))

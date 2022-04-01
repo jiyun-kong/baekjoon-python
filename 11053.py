edad171df883143d61442e15c2563b7ac3c16118
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-04-01 16:24:33 +0900

    가장 긴 증가하는 부분 수열

--- 11053.py
+++ 11053.py
@@ -1,25 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-seq = list(map(int, input().split(' ')))
-dp = [[] for _ in range(n)]
+N = int(input())
 
-if n == 1:
-    print(1)
-else:
-    for i in range(n):
-        dp[i].append(seq[i])
+A = list(map(int, input().split()))
 
-        for j in range(i+1, n):
-            if dp[i][-1] < seq[j]:
-                dp[i].append(seq[j])
-    # print(dp)
-    max = 1
-    for i in range(n):
-        if max < len(dp[i]):
-            max = len(dp[i])
+dp = [1] * N
 
-    print(max)
+for i in range(N):
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-# 반례 : n = 6 / 1 2 8 2 4 8
+print(max(dp))
